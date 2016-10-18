51c12c7a728d51e0db0e2834514f352a61f88f05
Fizal <fizalihsan@gmail.com>
2016-10-17 22:08:04 -0400

    more learning

--- src/main/org/fizal/a_datatypes/i_CollectionUtils.py
+++ src/main/org/fizal/a_datatypes/i_CollectionUtils.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from collections import namedtuple
+
 # ------------------Zip------------------
 # zip takes in iterables as input and returns list of tuples
 s = 'abc'
@@ -12,3 +15,12 @@ s = 'abc'
 l = [1, 2, 3]
 t = (4, 5, 6)
 print zip(s, l, t)  # [('a', 1, 4), ('b', 2, 5), ('c', 3, 6)]
+
+# ------------------Counter------------------
+count = Counter('parrot')
+print count  # Counter({'r': 2, 'a': 1, 'p': 1, 't': 1, 'o': 1})
+
+# ------------------Named tuple------------------
+Point = namedtuple('Point', ['x', 'y'])
+p = Point(x=10, y=20)
+print p # Point(x=10, y=20)
--- src/main/org/fizal/c_functions/02-Varargs.py
+++ src/main/org/fizal/c_functions/02-Varargs.py
@@ -1,3 +1,5 @@
+from src.main.org.fizal.d_classes.Point import Point
+# -------------------------Gather-------------------------
 def printall(*args):
     print type(args)  # tuple
     for arg in args:
@@ -7,6 +9,15 @@ def printall(*args):
 printall(1, 2, 3, 4)  # all the args are 'gathered' as single arg
 
 
+# to gather keywords
+def printall_with_keywords(*args, **kwargs):
+    print args, kwargs
+
+
+printall_with_keywords(1, 2, a=3, c=4)
+
+
+# -------------------------Scatter-------------------------
 def scatter(a, b, c):
     print a
     print b
@@ -15,3 +26,8 @@ def scatter(a, b, c):
 
 tuple = (4, 5, 6)
 scatter(*tuple)  # all the args are 'scattered into multiple args here
+
+# Scatter operator **
+d = dict(x=1, y=2)
+p = Point(**d)
+print p # x=01 y=02
